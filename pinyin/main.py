ca352f52b5cfdfc6d490955079a12f39ba87a1cd
WooParadog <guohaochuan@gmail.com>
2012-05-02 10:04:05 +0800

    fix

--- pinyin/main.py
+++ pinyin/main.py
@@ -11,7 +11,21 @@ from pinyin2 import Pinyin
 import doctest
 
 parser = Pinyin()
-pinyin_list_by_str = parser.get_pinyin
+head = {}
+with open("name") as f:
+    for line in f:
+        name, pinyin = line.strip().decode('utf8').split()
+        head[name] = pinyin
+
+def pinyin_list_by_str(name):
+    first = name[0]
+    if first in head:
+        result = [head[first]] + parser.get_pinyin(name[1:])
+    else:
+        result = parser.get_pinyin(name)
+
+    return result
+
 
 def pinyin_parser_length_2(name):
     pinyin_list = pinyin_list_by_str(name)
@@ -31,14 +45,16 @@ def pinyin_parser_length_x(name):
 
 def pinyin_parser(name):
     '''
-    >>> pinyin_parser("郇豪")
-    'xuha'
+    >>> pinyin_parser("曾蔷")
+    u'zeqi'
     >>> pinyin_parser("郭浩川")
-    'ghch'
+    u'ghch'
     >>> pinyin_parser("郭浩慧川")
-    'ghhc'
+    u'ghhc'
     >>> pinyin_parser("王浩郭慧川")
-    'whgh'
+    u'whgh'
+    >>> pinyin_parser("郇豪")
+    u'xuha'
     '''
     name = name.decode('utf8')
 
@@ -51,7 +67,7 @@ def pinyin_parser(name):
     elif len(name) > 4:
         result = pinyin_parser_length_x(name)
 
-    return result[:4]
+    return unicode(result[:4])
 
 def main():
     doctest.testmod()
